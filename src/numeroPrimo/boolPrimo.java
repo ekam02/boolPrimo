c6bee9f8118601749d01376c2180670dc257f228
ekam02 <ekam02@gmail.com>
2016-09-09 12:00:26 -0500

    Cambio de nombre

--- src/numeroPrimo/boolPrimo.java
+++ src/numeroPrimo/boolPrimo.java
@@ -2,7 +2,7 @@
  * Programa en Java que dado un número entero positivo determina si es número
  * primo o no.
  */
-package numero.primo.o.no;
+package numeroPrimo;
 
 import java.util.Scanner;
 
@@ -10,7 +10,7 @@ import java.util.Scanner;
  *
  * @author Enrique A. Martínez Agudelo
  */
-public class NumeroPrimoONo {
+public class boolPrimo {
 
     /**
      * @param args the command line arguments
